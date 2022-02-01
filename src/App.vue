dd546b91f5154a3eac1e0bd8446066983b442ac1
gabrielaabergamo <gabiibergamo@gmail.com>
2022-02-01 16:58:35 -0300

    setup inicial do projeto

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Hi there</div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar.vue";
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
